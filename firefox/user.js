5165ac5e21d066e5d61b39eabdab031ce8a50ed4
Gustaf Granström <gustaf@granstrm.com>
2026-06-14 04:41:27 +0200

    configure firefox

---
+++ firefox/user.js
@@ -0,0 +1,21 @@
+// Restore previous session
+user_pref("browser.startup.page", 3);
+
+// Middle mouse scroll
+user_pref("general.autoScroll", true);
+
+// Enable browser toolbox
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
+
+user_pref("browser.sessionstore.resume_from_crash", true);
+user_pref("general.aboutConfig.enable", true);
+user_pref("middlemouse.paste", false);
+user_pref("sidebar.animation.enabled", false);
+// user_pref("image.jxl.enabled", true);
+user_pref("devtools.cache.disabled", true);
+user_pref("devtools.netmonitor.persistlog", true);
+user_pref("devtools.webconsole.persistlog", true);
+user_pref("devtools.webconsole.timestampMessages", true);
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-alias ln='ln -Tsrf'
+alias ln='ln -Tsrfv'
 
 ln dunst ~/.config/dunst
 ln hypr ~/.config/hypr
@@ -8,3 +8,12 @@ ln walker ~/.config/walker
 ln waybar ~/.config/waybar
 ln elephant ~/.config/elephant
 ln nvim ~/.config/nvim
+if [ -f ~/.config/mozilla/firefox/profiles.ini ]; then
+  profile="$(yq -r '[to_entries[] | select(.key | test("^Install"))][0].value.Default' ~/.config/mozilla/firefox/profiles.ini)"
+  profilePath=~/.config/mozilla/firefox/"$profile"
+  mkdir -vp "$profilePath/chrome"
+  ln firefox/userChrome.css "$profilePath/chrome/userChrome.css"
+  ln firefox/user.js "$profilePath/user.js"
+else
+  echo "Warning: Firefox profiles.ini not found, start Firefox once and rerun the script." >&2
+fi
